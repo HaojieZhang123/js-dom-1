75d810ac99099207def1a41430e066a284c6628f
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-25 13:28:05 +0100

    [UPD] base excercise done. lamp will light up when button is clicked

--- js/script.js
+++ js/script.js
@@ -1,2 +1,6 @@
-const lamp = document.getElementById('.lamp');
+const lamp = document.getElementById('lamp');
 const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    lamp.src = './img/yellow_lamp.png';
+});
